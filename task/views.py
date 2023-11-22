5660c4507631cd23ef825aac75366c6067b52011
Yuliia Hladyshkevych <yulia.gladyshkevych@mail.com>
2023-11-22 16:32:47 +0200

    Added main page with information about tasks, task types, positions and workers

--- task/views.py
+++ task/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from task.models import TaskType, Task, Position, Worker
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_task_types = TaskType.objects.count()
+    num_tasks = Task.objects.count()
+    num_positions = Position.objects.count()
+    num_workers = Worker.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_task_types": num_task_types,
+        "num_tasks": num_tasks,
+        "num_positions": num_positions,
+        "num_workers": num_workers,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task/index.html", context=context)
