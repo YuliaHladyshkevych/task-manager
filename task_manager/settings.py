2304e2de607d7ef91302e635198475bcde6bc53f
Yuliia Hladyshkevych <yulia.gladyshkevych@mail.com>
2023-11-24 20:26:28 +0200

    add https://task-manager-2cr9.onrender.com to ALLOWED_HOSTS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANG0_SECRET_KEY"]
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "task-manager-2cr9.onrender"]
 
 
 # Application definition
