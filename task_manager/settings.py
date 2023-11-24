0c934374ac52c539aa8224d683d1c711289fb207
Yuliia Hladyshkevych <yulia.gladyshkevych@mail.com>
2023-11-24 20:38:12 +0200

    update ALLOWED_HOSTS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "task-manager-2cr9.onrender"]
+ALLOWED_HOSTS = ["127.0.0.1", "task-manager-2cr9.onrender.com"]
 
 
 # Application definition
